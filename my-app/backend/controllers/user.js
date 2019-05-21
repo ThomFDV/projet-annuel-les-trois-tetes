77620cede80e7cf2f08a7c3109ef59669e53d31f
ThomFDV <t.ferreira@monsieurbiz.com>
2019-05-21 15:00:31 +0200

    Update user creation to have a crypted password

--- my-app/backend/controllers/user.js
+++ my-app/backend/controllers/user.js
@@ -1,13 +1,25 @@
+const bcrypt = require("bcrypt");
+
 const User = require("../models/user");
 
 exports.createUser = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
     const user = new User({
       email: req.body.email,
-      password: req.body.password
+      password: hash
     });
-    user.save(function (err, book) {
-      if (err) return console.error(err);
-      console.log(user.email + " saved to the collection.");
-    });
-    return res.json(user).status(201).end();
+    user
+        .save()
+        .then(result => {
+          res.status(201).json({
+            message: "User created!",
+            result: result
+          });
+        })
+        .catch(err => {
+          res.status(500).json({
+            message: "Invalid authentication credentials!"
+          });
+        });
+  });
 };
